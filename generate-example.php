2871c82b1562086e39e1b8366874497689c53839
Ondrej Esler <esler.ondrej@gmail.com>
2018-03-29 11:53:45 +0200

    bugfix

--- generate-example.php
+++ generate-example.php
@@ -10,6 +10,6 @@ $log->pushHandler(new StreamHandler(STDOUT, Logger::DEBUG));
 
 $levels = array_map('strtolower', array_keys(Logger::getLevels()));
 
-for ($i = 0; $i < $argv[1] ?? 10; $i++) {
+for ($i = 0; $i < ($argv[1] ?? 10); $i++) {
     $log->{$levels[array_rand($levels)]}('Test message');
 }
